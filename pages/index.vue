6899127d3b7090b96a78303dc3d83afbc7fc4229
Maiwenn <le-goasteller.e2000660@etud.univ-ubs.fr>
2022-03-16 16:57:41 +0100

    reparation

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<script>
+
+let id = 0
+
+export default {
+    data() {
+        return {
+            todos: [
+                { id: id++, text: "don't worry"},
+                { id: id++, text: "be happy"}
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Salut, bienvenue sur ta Todo-List !</h1>
+        <ul>
+            <li v-for="todo in todos" :key="todo.id">
+                {{ todo.text }}
+            </li>
+        </ul>
+
+    </div>
+ </template>
